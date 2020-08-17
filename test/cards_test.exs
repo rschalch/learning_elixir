691d17c38252d34a44cb3049524841bc37261ff3
rbs <ricardo.schalch@gmail.com>
2020-08-17 01:43:34 -0300

    add one more test case

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -5,4 +5,9 @@ defmodule CardsTest do
   test "create_deck makes 36 cards" do
     assert length(Cards.create_deck) == 36
   end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
 end
